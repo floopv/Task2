e7300125c8a40768526163e0fef6611c05f3598f
felob <felob@FLOOPV>
2025-07-29 19:12:05 +0300

    2nd part of task (loops)

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,26 +6,38 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-        //task: Grade Checker
-            int MinGrade = 0;
-            int MaxGrade = 100;
-            Console.WriteLine($"Please Enter your mark from {MinGrade} to {MaxGrade} :");
-            double grade = Convert.ToDouble(Console.ReadLine());
-            if (grade >= MinGrade && grade <= MaxGrade)
+            //task: Grade Checker
+            //        int MinGrade = 0;
+            //        int MaxGrade = 100;
+            //        Console.WriteLine($"Please Enter your mark from {MinGrade} to {MaxGrade} :");
+            //        double grade = Convert.ToDouble(Console.ReadLine());
+            //        if (grade >= MinGrade && grade <= MaxGrade)
+            //        {
+            //            if (grade >= 90 && grade <= 100)
+            //                Console.WriteLine("Your Grade is A");
+            //            else if (grade >= 80 && grade <=89)
+            //                Console.WriteLine("Your Grade is B");
+            //            else if (grade >= 70 && grade <= 79)
+            //                Console.WriteLine("Your Grade is C");
+            //            else if (grade >= 60 && grade <= 69)
+            //                Console.WriteLine("Your Grade is D");
+            //            else
+            //                Console.WriteLine("Your Grade is F");
+            //        }
+            //        else 
+            //            Console.WriteLine("You Entered Wrong Value");
+
+            //=====================================================================
+
+            //Multiplication Table:
+
+            Console.WriteLine("Please Enter a number to print its multiplication table :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 13; i++)
             {
-                if (grade >= 90 && grade <= 100)
-                    Console.WriteLine("Your Grade is A");
-                else if (grade >= 80 && grade <=89)
-                    Console.WriteLine("Your Grade is B");
-                else if (grade >= 70 && grade <= 79)
-                    Console.WriteLine("Your Grade is C");
-                else if (grade >= 60 && grade <= 69)
-                    Console.WriteLine("Your Grade is D");
-                else
-                    Console.WriteLine("Your Grade is F");
+                Console.WriteLine($"{number} * {i} = {number * i} ");
+
             }
-            else 
-                Console.WriteLine("You Entered Wrong Value");
         }
     }
-}
+    }
